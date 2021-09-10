002adb7d66475c5eb4b631fe7b5d6fcbfd9e1a59
hackzzdogs1337 <57171267+hackzzdogs1337@users.noreply.github.com>
2021-09-10 21:59:00 +0530

    Update settings.py

--- krakenweb/krakenweb/settings.py
+++ krakenweb/krakenweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ru@b=zyv%qx-12)xs&^u4hv+-(4!si#)o0s^nh0l&3%*!qk)%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
